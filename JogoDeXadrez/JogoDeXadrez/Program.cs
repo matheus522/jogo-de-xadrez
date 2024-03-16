fa3fd10308a1a31e22b2dfa25605e6feea62b3cf
Rissi <mrissi2901@gmail.com>
2024-03-16 14:14:07 -0300

    New Commit
    Classe Position

--- JogoDeXadrez/JogoDeXadrez/Program.cs
+++ JogoDeXadrez/JogoDeXadrez/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace JogoDeXadrez
+﻿using JogoDeXadrez.Tabuleiro;
+
+namespace JogoDeXadrez
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Posicao p = new Posicao(3,4);
+            Console.WriteLine(p);
+
         }
     }
 }
---
+++ JogoDeXadrez/JogoDeXadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace JogoDeXadrez.Tabuleiro
+{
+    internal class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set;}
+
+
+        public Posicao() { }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Linha}, {Coluna}");
+
+            return sb.ToString();
+        }
+    }
+}
